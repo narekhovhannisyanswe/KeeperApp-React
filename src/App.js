044d056eb51b8be89dd419183822dc2704bd0cbc
Narek Hovhannisyan <narek.hovhannisyan.swe@gmail.com>
2023-05-16 15:12:33 +0400

    add Header, Footer, Note and Notes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
+import Header from "./compoments/Header";
+import Footer from "./compoments/Footer";
+import Notes from "./compoments/Notes";
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <Notes />
+      <Footer />
     </div>
   );
 }
